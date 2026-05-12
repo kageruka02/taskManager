7c156b880a64ff08817a6a1dc3ae954d7600e9ec
KAGERUK02 <leonkageruka@gmail.com>
2026-05-12 23:03:50 +0200

    Add unit tests for TaskService

--- src/test/java/com/example/taskmanager/service/TaskServiceTest.java
+++ src/test/java/com/example/taskmanager/service/TaskServiceTest.java
@@ -1,4 +1,30 @@
+package com.example.taskmanager.service;
+
+import com.example.taskmanager.model.Task;
+import com.example.taskmanager.repository.TaskRepository;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class TaskServiceTest {
-  
+
+    private final TaskRepository repository = Mockito.mock(TaskRepository.class);
+
+    private final TaskService service = new TaskService(repository);
+
+    @Test
+    void shouldReturnAllTasks() {
+
+        Task task = new Task(1L, "Study DevOps", "IN_PROGRESS");
+
+        Mockito.when(repository.findAll()).thenReturn(List.of(task));
+
+        List<Task> tasks = service.getAllTasks();
+
+        assertEquals(1, tasks.size());
+        assertEquals("Study DevOps", tasks.get(0).getTitle());
+    }
 }
